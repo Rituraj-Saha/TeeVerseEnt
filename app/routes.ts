5b04f56716f2810779a5e032235fc6438134a24b
Rituraj Saha <rituraj10saha@gmail.com>
2025-05-10 16:30:36 +0530

    build: building in progress

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
-
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+const appname = import.meta.env.APP_NAME;
+console.log(appname);
+export default [
+  //   index("routes/home.tsx"),
+  route(`${"/dev"}`, "routes/home.tsx"),
+] satisfies RouteConfig;
